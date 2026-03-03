49cef0813180d9807fe7d19d6b803efd8bda069e
SamadhanBheke05 <bhekesamadhan@gmail.com>
2026-03-03 13:46:24 +0530

    backend1

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -14,7 +14,7 @@ const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        pass: (process.env.EMAIL_PASS || "").replace(/\s+/g, "")
     }
 });
 
@@ -114,3 +114,4 @@ export const sendGroupBookingConfirmationEmail = async ({
         `,
     });
 };
+
